58ba618d9e3d83bf8a1d222aa3b0ca1a553b42f1
allyjfuller <allyjfuller@gmail.com>
2019-10-17 21:57:04 -0400

    changed netlify address

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Alexandra Fuller',
   shortName: 'Fuller',
   description: 'An artist turned software engineer, raised in the travel and hospitality industry with an eye for design and an entrepreneurial spirit',
-  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  url: 'https://sharp-mirzakhani-507a86.netlify.com/', // Domain of your site without prefix!
   prefix: '/',
   author: '@allyjfuller',
   github: 'allyjfuller'
